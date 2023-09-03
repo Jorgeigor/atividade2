dab84fa120103e342910145bdfba000dd9ee5986
Jorgeigor <142018986+Jorgeigor@users.noreply.github.com>
2023-09-03 16:03:57 -0300

    Add files via upload

---
+++ EXERCICIO1.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class EXERCICIO1 {
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        System.out.print("Escolha a operação (+, -, *, /): ");
+        char operacao = scanner.next().charAt(0);
+
+        double resultado = 0.0;
+
+        switch (operacao) {
+            case '+':
+                resultado = numero1 + numero2;
+                break;
+            case '-':
+                resultado = numero1 - numero2;
+                break;
+            case '*':
+                resultado = numero1 * numero2;
+                break;
+            case '/':
+                if (numero2 != 0) {
+                    resultado = numero1 / numero2;
+                } else {
+                    System.out.println("Erro: Divisão por zero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Operação inválida.");
+                return;
+        }
+
+        System.out.println("Resultado: " + resultado);
+    }
+}
